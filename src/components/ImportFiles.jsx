ca70493529d3b23bf8fae58cc51569e9b5a7520e
samarpit sharma <SASharma@livegage.ai>
2024-02-19 11:54:19 +0530

    added import functionality and export button

---
+++ src/components/ImportFiles.jsx
@@ -0,0 +1,67 @@
+import React, { useCallback, useState } from "react";
+import { Button } from "react-bootstrap";
+import Papa from "papaparse";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
+
+const ImportFiles = () => {
+  const [alertMessage, setAlertMessage] = useState(null);
+  const showAlert = (variant, message) => {
+    setAlertMessage({ variant, message });
+  };
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      Papa.parse(file, {
+        header: true,
+        skipEmptyLines: true,
+        transformHeader: function (header) {
+          return header.replace(/\s+/g, "");
+        },
+        complete: (result) => {
+          const formattedData = result.data.map((row) => {
+            if (row.PossibleValues && row.PossibleValues.trim() !== "") {
+              row.PossibleValues = row.PossibleValues.split(",");
+            } else {
+              row.PossibleValues = null;
+            }
+
+            if (row.Synonyms && row.Synonyms.trim() !== "") {
+              row.Synonyms = row.Synonyms.split(",");
+            } else {
+              row.Synonyms = null;
+            }
+            return row;
+          });
+
+          axios
+            .post("http://localhost:5000/api/dictionary/Bulk", formattedData)
+            .then((response) => {
+              showAlert("success", "CSV Data Added Successfully!");
+            })
+            .catch((error) => {
+              console.log(error);
+              if (error.response && error.response.status === 400) {
+                alert(
+                  "Your Data is not Saved. \nThere are errors in the data.\n Please review and correct them."
+                );
+              }
+
+              showAlert("danger", "Error adding CSV data.");
+            });
+        },
+        // },
+      });
+    });
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  return (
+    <Button {...getRootProps()}>
+      <input {...getInputProps()} />
+      Import
+    </Button>
+  );
+};
+
+export default ImportFiles;
--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import ImportFiles from "../components/ImportFiles";
+import ExportFIles from "../components/ExportFIles";
 
 const HomePage = () => {
   return (
     <Container className="text-center">
       <h1>Data Dictionary</h1>
+      <ImportFiles />
+      <ExportFIles />
     </Container>
   );
 };
