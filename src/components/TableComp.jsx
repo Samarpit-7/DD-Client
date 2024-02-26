a437d11e80d96685718c21d7a756ac9ade3ee98e
Mustafa-livegage <mpanjiwala@livegage.ai>
2024-02-26 11:43:51 +0530

    Created table component

---
+++ src/components/TableComp.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { Button, Table } from "react-bootstrap";
+
+const TableComp = ({ data, columnMapping }) => {
+  if (!data || data.length === 0) {
+    return <div>No data available</div>;
+  }
+
+  const columns = Object.keys(columnMapping);
+  const [expandedRows, setExpandedRows] = useState([]);
+
+  const toggleRowExpansion = (rowIndex) => {
+    if (expandedRows.includes(rowIndex)) {
+      setExpandedRows(expandedRows.filter((row) => row !== rowIndex));
+    } else {
+      setExpandedRows([...expandedRows, rowIndex]);
+    }
+  };
+
+  const renderCellContent = (row, columnKey, rowIndex) => {
+    if (Array.isArray(row[columnKey])) {
+      if (row[columnKey].length > 5) {
+        // Assuming the limit of displaying possible values is 5
+        return (
+          <div>
+            {expandedRows.includes(rowIndex)
+              ? row[columnKey].join(", ")
+              : row[columnKey].slice(0, 5).join(", ")}
+            <br />
+            {expandedRows.includes(rowIndex) ? (
+              <Button
+                variant="link"
+                size="sm"
+                onClick={() => toggleRowExpansion(rowIndex)}
+              >
+                Read less
+              </Button>
+            ) : (
+              <Button
+                variant="link"
+                size="sm"
+                onClick={() => toggleRowExpansion(rowIndex)}
+              >
+                Read more
+              </Button>
+            )}
+          </div>
+        );
+      } else {
+        return row[columnKey].join(", ");
+      }
+    } else if (
+      columnKey === "definition" ||
+      columnKey === "fieldType" ||
+      columnKey === "container" ||
+      columnKey === "dataPoint" ||
+      columnKey === "dbColumnName" ||
+      columnKey === "dbDataType" ||
+      columnKey === "calculatedInfo"
+    ) {
+      if (row[columnKey].length > 25) {
+        return (
+          <div>
+            {expandedRows.includes(rowIndex)
+              ? row[columnKey]
+              : `${row[columnKey].substring(0, 25)}...`}
+            <br />
+            {expandedRows.includes(rowIndex) ? (
+              <Button
+                variant="link"
+                size="sm"
+                onClick={() => toggleRowExpansion(rowIndex)}
+              >
+                Read less
+              </Button>
+            ) : (
+              <Button
+                variant="link"
+                size="sm"
+                onClick={() => toggleRowExpansion(rowIndex)}
+              >
+                Read more
+              </Button>
+            )}
+          </div>
+        );
+      } else {
+        return row[columnKey];
+      }
+    } else {
+      return row[columnKey];
+    }
+  };
+
+  return (
+    <Table striped responsive bordered className="overflow-scroll ">
+      <thead className="table-dark">
+        <tr>
+          {columns.map((columnKey, index) => (
+            <th key={index}>{columnMapping[columnKey]}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {columns.map((columnKey, colIndex) => (
+              <td key={colIndex}>
+                {renderCellContent(row, columnKey, rowIndex)}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default TableComp;
--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import ImportFiles from "../components/ImportFiles";
 import ExportFIles from "../components/ExportFIles";
+import TableComp from "../components/TableComp";
+import axios from "axios";
 
 const HomePage = () => {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/dictionary")
+      .then(function (response) {
+        setApiData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const columnMapping = {
+    container: "Container",
+    dataPoint: "Data Point",
+    dbColumnName: "DB Column Name",
+    fieldType: "Field Type",
+    dbDataType: "DB Data Type",
+    definition: "Definition",
+    possibleValues: "Possible Values",
+    synonyms: "Synonyms",
+    calculatedInfo: "Calculated Info",
+  };
   return (
-    <Container className="text-center">
+    <Container className="text-center container-fluid p-0 w-100">
       <h1>Data Dictionary</h1>
-      <ImportFiles />
-      <ExportFIles />
+      {/* <ImportFiles />
+      <ExportFIles /> */}
+      <div>
+        <TableComp data={apiData} columnMapping={columnMapping} />
+      </div>
     </Container>
   );
 };
